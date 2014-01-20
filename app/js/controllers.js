bae3e7bd131252308e1bb5964cb7ccf57343b624
Baptiste Gouby <baptiste.gouby@gmail.com>
2014-01-20 10:08:03 -0500

    Added font-awesome + Created FileExplorerController + Chart.js and Notifications in bottom panel

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,24 @@
-/**
- * Created by baptiste on 1/19/2014.
- */
+var CubbyHoleApp = angular.module('cubbyholeApp', []);
+
+CubbyHoleApp.controller('FileExplorerCtrl', function ($scope) {
+    $scope.files = [
+        {   'name': 'My picture 1.png',
+            'owner': 'Baptiste Gouby',
+            'size': '150 Kb'            },
+        {   'name': 'My picture 2.png',
+            'owner': 'Brian',
+            'size': '90 Kb'            },
+        {   'name': 'My picture 3.png',
+            'owner': 'Brian',
+            'size': '637 Kb'            },
+        {   'name': 'My picture 4.png',
+            'owner': 'Brian',
+            'size': '453 Kb'            },
+        {   'name': 'My file.pdf',
+            'owner': 'Peter Griffin',
+            'size': '8 Mb'              },
+        {   'name': 'Vacations.avi',
+            'owner': 'Peter Griffin',
+            'size': '874 Mb'            }
+    ];
+});
