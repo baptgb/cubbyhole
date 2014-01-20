2ffbab8aa1b41a04af4059252947ad3eb80cb50a
Baptiste Gouby <baptiste.gouby@gmail.com>
2014-01-20 11:08:33 -0500

    Added Monitoring Controller (Usage + Notifications modules)

--- app/js/app.js
+++ app/js/app.js
@@ -1,73 +1,16 @@
 
 $(document).ready(function(){
-
     $(".close").click( function(e) {
         console.log(e);
-        $(this).parent().parent().fadeOut();
+        $(this).parent().fadeOut();
     });
-    var data = [
-        {
-            value: 30,
-            color:"#F7464A"
-        },
-        {
-            value : 50,
-            color : "#E2EAE9"
-        },
-        {
-            value : 100,
-            color : "#D4CCC5"
-        },
-        {
-            value : 40,
-            color : "#949FB1"
-        },
-        {
-            value : 120,
-            color : "#4D5360"
-        }
-
-    ];
-    var options = {
-        //Boolean - Whether we should show a stroke on each segment
-        segmentShowStroke : true,
-
-        //String - The colour of each segment stroke
-        segmentStrokeColor : "#fff",
-
-        //Number - The width of each segment stroke
-        segmentStrokeWidth : 4,
-
-        //The percentage of the chart that we cut out of the middle.
-        percentageInnerCutout : 40,
-
-        //Boolean - Whether we should animate the chart
-        animation : true,
-
-        //Number - Amount of animation steps
-        animationSteps : 100,
-
-        //String - Animation easing effect
-        animationEasing : "easeOutBounce",
-
-        //Boolean - Whether we animate the rotation of the Doughnut
-        animateRotate : true,
-
-        //Boolean - Whether we animate scaling the Doughnut from the centre
-        animateScale : false,
-
-        //Function - Will fire on animation completion.
-        onAnimationComplete : null
-    };
-    var ctx = document.getElementById('usage-chart').getContext("2d");
-    new Chart(ctx).Doughnut(data,options);
 });
 
 /* App Module */
 
 var CubbyHoleApp = angular.module('cubbyholeApp', [
     'ngRoute',
-    'cubbyholerControllers'
+    'cubbyholeControllers'
 ]);
 
 CubbyHoleApp.config(['$routeProvider',
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,4 @@
-var CubbyHoleApp = angular.module('cubbyholeApp', []);
+var CubbyHoleControllers = angular.module('cubbyholeControllers', []);
 
 CubbyHoleApp.controller('FileExplorerCtrl', function ($scope) {
     $scope.files = [
@@ -20,5 +20,50 @@ CubbyHoleApp.controller('FileExplorerCtrl', function ($scope) {
         {   'name': 'Vacations.avi',
             'owner': 'Peter Griffin',
             'size': '874 Mb'            }
+    ];
+});
+
+CubbyHoleApp.controller('MonitorCtrl', function ($scope) {
+
+    // Chart module
+    $scope.chartData = [
+        {   value: 30,
+            color:"#F7464A" },
+        {   value : 70,
+            color : "#4D5360"   }
+    ];
+    $scope.chartOptions = {
+        //Boolean - Whether we should show a stroke on each segment
+        segmentShowStroke : true,
+        //String - The colour of each segment stroke
+        segmentStrokeColor : "#fff",
+        //Number - The width of each segment stroke
+        segmentStrokeWidth : 4,
+        //The percentage of the chart that we cut out of the middle.
+        percentageInnerCutout : 40,
+        //Boolean - Whether we should animate the chart
+        animation : true,
+        //Number - Amount of animation steps
+        animationSteps : 100,
+        //String - Animation easing effect
+        animationEasing : "easeOutBounce",
+        //Boolean - Whether we animate the rotation of the Doughnut
+        animateRotate : true,
+        //Boolean - Whether we animate scaling the Doughnut from the centre
+        animateScale : false,
+        //Function - Will fire on animation completion.
+        onAnimationComplete : null
+    };
+    var ctx = document.getElementById('usage-chart').getContext("2d");
+    $scope.chart = new Chart(ctx).Doughnut( $scope.chartData, $scope.chartOptions);
+
+    // Notification module
+    $scope.notifications = [
+        {   'title': 'First notification',
+            'content': 'You\'ve got a notification' },
+        {   'title': 'Second notification',
+            'content': 'You\'ve got the second notification' },
+        {   'title': 'Third notification',
+            'content': 'You\'ve got the third notification' }
     ];
 });
