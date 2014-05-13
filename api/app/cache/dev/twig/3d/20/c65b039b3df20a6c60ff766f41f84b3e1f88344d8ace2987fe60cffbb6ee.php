27d5a7be9647c28c65727ed1870b0985eb588e4b
Baptiste Gouby <baptiste.gouby@gmail.com>
2014-05-12 22:11:52 -0400

    Merge dev-baptiste

---
+++ api/app/cache/dev/twig/3d/20/c65b039b3df20a6c60ff766f41f84b3e1f88344d8ace2987fe60cffbb6ee.php
@@ -0,0 +1,58 @@
+<?php
+
+/* TwigBundle:Exception:trace.txt.twig */
+class __TwigTemplate_3d20c65b039b3df20a6c60ff766f41f84b3e1f88344d8ace2987fe60cffbb6ee extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        if ($this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "function")) {
+            // line 2
+            echo "    at ";
+            echo (($this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "class") . $this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "type")) . $this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "function"));
+            echo "(";
+            echo $this->env->getExtension('code')->formatArgsAsText($this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "args"));
+            echo ")
+";
+        } else {
+            // line 4
+            echo "    at n/a
+";
+        }
+        // line 6
+        if (($this->getAttribute((isset($context["trace"]) ? $context["trace"] : null), "file", array(), "any", true, true) && $this->getAttribute((isset($context["trace"]) ? $context["trace"] : null), "line", array(), "any", true, true))) {
+            // line 7
+            echo "        in ";
+            echo $this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "file");
+            echo " line ";
+            echo $this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "line");
+            echo "
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:trace.txt.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  33 => 6,  21 => 2,  39 => 8,  29 => 4,  26 => 3,  22 => 2,  51 => 9,  44 => 7,  42 => 6,  35 => 7,  31 => 5,  25 => 3,  19 => 1,);
+    }
+}
